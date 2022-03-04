a955f86bc809b517e88d6e0527957ee5435e84ff
Filip Budiša <filip@circuitmess.com>
2022-03-04 14:52:50 +0100

    Add logging init to .ino

--- Synthia-Firmware.ino
+++ Synthia-Firmware.ino
@@ -1,9 +1,23 @@
 #include <Arduino.h>
-#include <CMAudio.h>
+#include <Synthia.h>
 #include <CircuitOS.h>
+#include <CMAudio.h>
+
+#include <esp_log.h>
+
+void initLog(){
+	esp_log_level_set("*", ESP_LOG_NONE);
+
+	const static auto tags = { "Synthia" };
+
+	for(const char* tag : tags){
+		esp_log_level_set(tag, ESP_LOG_VERBOSE);
+	}
+}
 
 void setup(){
-
+	Synthia.begin();
+	initLog();
 }
 
 void loop(){
