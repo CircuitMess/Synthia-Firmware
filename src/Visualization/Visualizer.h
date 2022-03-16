1d4673149dad624bd3904e668ad097bf14204560
Filip Budiša <filip@circuitmess.com>
2022-03-16 20:03:59 +0100

    Add Visualizer system (#7)
    * Add Visualizer system
    
    * Add empty implementations for VisualizerBase onStart and onStop
    
    * Minor fixes for Visualizer
    
    Co-authored-by: Emil Gajšak <emil.gajsak@gmail.com>

---
+++ src/Visualization/Visualizer.h
@@ -0,0 +1,89 @@
+#ifndef SYNTHIA_FIRMWARE_VISUALIZER_H
+#define SYNTHIA_FIRMWARE_VISUALIZER_H
+
+#include <Arduino.h>
+#include <CircuitOS.h>
+#include <Loop/LoopListener.h>
+
+class VisualizerBase;
+
+class VisualizerManager : public LoopListener {
+friend VisualizerBase;
+public:
+	void begin();
+	void loop(uint32_t time) override;
+
+private:
+	VisualizerBase* main = nullptr;
+	VisualizerBase* current = nullptr;
+
+	void setMain(VisualizerBase* main);
+	void pushing(VisualizerBase* vis);
+
+	uint32_t pushTime = 0;
+
+};
+
+extern VisualizerManager VMan;
+
+class VisualizerBase {
+friend VisualizerManager;
+public:
+	virtual ~VisualizerBase(){
+		free(prop);
+	}
+
+	void push();
+
+	void setMain();
+
+protected:
+	template<typename T>
+	void push(const T& value){
+		if(propSize == 0){
+			propSize = sizeof(T);
+			prop = malloc(propSize);
+		}else if(propSize != sizeof(T)){
+			return;
+		}
+
+		memcpy(prop, &value, propSize);
+
+		push();
+	}
+
+	template<typename T>
+	const T& getProp(){
+		return *static_cast<T*>(prop);
+	}
+
+	virtual void visualize() = 0;
+
+	virtual void onStart();
+	virtual void onStop();
+
+private:
+	void* prop = nullptr;
+	size_t propSize = 0;
+
+	bool started = false;
+
+	void start();
+	void stop();
+};
+
+template<typename T>
+class Visualizer : public VisualizerBase {
+public:
+	void push(const T& value){
+		VisualizerBase::push(value);
+	}
+
+protected:
+	const T& getProp(){
+		return VisualizerBase::getProp();
+	}
+
+};
+
+#endif
