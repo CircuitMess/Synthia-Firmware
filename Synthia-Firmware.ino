04229a18aaa0e070c4cddc55fec4445f2166be8c
Filip Budiša <filip@circuitmess.com>
2022-03-24 23:37:54 +0100

    Clean up .ino (Recorder-SYN-27)

--- Synthia-Firmware.ino
+++ Synthia-Firmware.ino
@@ -19,53 +19,10 @@ void initLog(){
 	}
 }
 
-//#include <Synthia.h>
-//#include <Pins.hpp>
-#include <JayD.h>
-#include <Input/InputJayD.h>
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-	Serial.printf("Listing directory: %s\n", dirname);
-
-	File root = fs.open(dirname);
-	if(!root){
-		Serial.println("Failed to open directory");
-		return;
-	}
-	if(!root.isDirectory()){
-		Serial.println("Not a directory");
-		return;
-	}
-
-	File file = root.openNextFile();
-	while(file){
-		if(file.isDirectory()){
-			Serial.print("  DIR : ");
-			Serial.println(file.name());
-			if(levels){
-				listDir(fs, file.name(), levels -1);
-			}
-		} else {
-			Serial.print("  FILE: ");
-			Serial.print(file.name());
-			Serial.print("  SIZE: ");
-			Serial.println(file.size());
-		}
-		file = root.openNextFile();
-	}
-}
-
-#include "src/AudioSystem/PlaybackSystem.h"
-#include "src/Services/SlotPlayer.h"
-#include <Loop/LoopManager.h>
-PlaybackSystem* ps;
-EditSlot* editSlot;
-uint8_t intensities[(uint8_t)EffectData::Type::COUNT] = {0};
-uint8_t speed;
-
 void setup(){
 	Serial.begin(115200);
-	Synthia.begin();
 	initLog();
+	Synthia.begin();
 
 	Playback.begin();
 	Player.begin();
