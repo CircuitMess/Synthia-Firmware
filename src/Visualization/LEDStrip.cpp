d9e0c7a6f82aa74e006b7ab2250c17f8d5fc7b0a
Emil Gajšak <emil.gajsak@gmail.com>
2022-03-28 18:21:43 +0200

    Minor fixes to LEDStrip

--- src/Visualization/LEDStrip.cpp
+++ src/Visualization/LEDStrip.cpp
@@ -15,36 +15,42 @@ void LEDStripImpl::setMidFill(uint8_t value){
 	uint32_t fill = map(value, 0, 255, 0, 255 * 16);
 	for(int i = 0; i < 16; ++i){
 		uint8_t pixelValue = min(fill, 255U);
-		CursorMatrix.drawPixel(i, {pixelValue, pixelValue, pixelValue, pixelValue});
+		Synthia.CursorMatrix.drawPixel(i, {255, 255, 255, pixelValue});
 		if(fill >= 255){
 			fill -= 255;
 		}else if(fill > 0){
 			fill = 0;
 		}
 	}
+	Synthia.CursorMatrix.push();
 }
 
 void LEDStripImpl::setMidSingle(uint8_t index){
-	CursorMatrix.drawPixel(index, {255, 255, 255, 255});
+	Synthia.CursorMatrix.clear();
+	Synthia.CursorMatrix.drawPixel(index, {255, 255, 255, 255});
+	Synthia.CursorMatrix.push();
 }
 
 void LEDStripImpl::setMidSelection(uint8_t value){
 	if(value > 1) return;
+	Synthia.CursorMatrix.clear();
 
 	for(int i = 0; i < 8; ++i){
-		CursorMatrix.drawPixel(i + value * 8, {255, 255, 255, 255});
+		Synthia.CursorMatrix.drawPixel(i + value * 8, {255, 255, 255, 255});
 	}
+	Synthia.CursorMatrix.push();
 }
 
 void LEDStripImpl::setStrip(uint8_t value, uint8_t x){
 	uint32_t fill = map(value, 0, 255, 0, 255 * 8);
 	for(int i = 7; i >= 0; --i){
 		uint8_t pixelValue = min(fill, 255U);
-		SlidersMatrix.drawPixel(x, i, {pixelValue, pixelValue, pixelValue, pixelValue});
+		Synthia.SlidersMatrix.drawPixel(x, i, {255, 255, 255, pixelValue});
 		if(fill >= 255){
 			fill -= 255;
 		}else if(fill > 0){
 			fill = 0;
 		}
 	}
+	Synthia.SlidersMatrix.push();
 }
