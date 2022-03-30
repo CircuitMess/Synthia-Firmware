3ce8974d0718517221e1e082d8dbf4dea5e4164f
Filip Budiša <filip@circuitmess.com>
2022-03-30 17:50:50 +0200

    Add State interface

---
+++ src/States/State.h
@@ -0,0 +1,26 @@
+#ifndef SYNTHIA_FIRMWARE_STATE_H
+#define SYNTHIA_FIRMWARE_STATE_H
+
+
+class State {
+public:
+	State(State* parent = nullptr);
+
+	void start();
+	void stop();
+
+	void push(State* parent);
+	void pop();
+
+protected:
+	virtual void onStart() = 0;
+	virtual void onStop() = 0;
+
+private:
+	State* parent = nullptr;
+	bool started = false;
+
+};
+
+
+#endif //SYNTHIA_FIRMWARE_STATE_H
