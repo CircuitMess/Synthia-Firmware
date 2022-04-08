60f1f190564f2ce6784bb53f4fa4a4e0bead7f40
Emil Gajšak <emil.gajsak@gmail.com>
2022-04-08 20:37:17 +0200

    Added TrackEditState

--- src/States/TrackEditState.cpp
+++ src/States/TrackEditState.cpp
@@ -1,16 +1,142 @@
+#include <cstdlib>
 #include "TrackEditState.h"
+#include "../Visualization/LEDStrip.h"
+#include "../AudioSystem/PlaybackSystem.h"
+#include "../SaveManager.h"
+#include "SaveState.h"
+
+uint8_t TrackEditState::cursor = 0;
+
+TrackEditState::TrackEditState(){
+	setTrack(saveManager.loadLast().track);
+}
 
 void TrackEditState::onStart(){
+	for(uint8_t i = 0; i < 5; i++){
+		setButtonHoldTime(i, 800);
+	}
+	setButtonHoldTime(BTN_ENC_R, 800);
+	setButtonHoldTime(BTN_ENC_L, 800);
 
+	cursor = 0;
+
+	LEDStrip.setLeft(track.tempo);
+	LEDStrip.setRight(Playback.getVolume());
+	pushTrackVis();
+	trackVis.setMain();
+
+	//TODO - start rgb slotAnim for track rgb leds
+	//Synthia.TrackRGB.startAnimation()
+
+	Input::getInstance()->addListener(this);
+	Encoders.addListener(this);
+	Sliders.addListener(this);
 }
 
 void TrackEditState::onStop(){
-
+	Input::getInstance()->removeListener(this);
+	Encoders.removeListener(this);
+	Sliders.removeListener(this);
+	VMan.clearMain();
 }
 
 void TrackEditState::setTrack(Track track){
+	this->track = track;
+	cursor = 0;
 }
 
-Track& TrackEditState::getTrack(){
+const Track& TrackEditState::getTrack(){
 	return track;
+}
+
+void TrackEditState::rightEncMove(int8_t amount){
+	for(uint& clickTime : clickTimes){
+		clickTime = 0;
+	}
+
+	if(cursor == 0 && amount < 0){
+		cursor = 15;
+	}else{
+		cursor = (cursor + amount) % 16;
+	}
+
+	pushTrackVis();
+}
+
+void TrackEditState::leftEncMove(int8_t amount){
+	auto save = new SaveState(this);
+	save->push(this);
+}
+
+void TrackEditState::leftPotMove(uint8_t value){
+	track.tempo = map(value, 0, 255, 60, 255);
+	tempoVis.push(track.tempo);
+}
+
+void TrackEditState::rightPotMove(uint8_t value){
+	Playback.setVolume(value);
+	volumeVis.push(value);
+}
+
+
+void TrackEditState::buttonHeld(uint i){
+	if(i < 5){
+		//TODO - open SampleEditState
+	}else if(i == BTN_ENC_R){
+		track.timeline.clear(cursor);
+		pushTrackVis();
+	}else if(i == BTN_ENC_L){
+		//TODO - open PlaybackState
+	}
+}
+
+void TrackEditState::click(uint8_t i){
+	if(i < 5){
+		track.timeline.set(cursor, i);
+		pushTrackVis();
+	}else if(i == BTN_ENC_R){
+		for(int j = 0; j < 5; ++j){
+			if(track.timeline.get(cursor, j)){
+				Playback.play(j);
+			}
+		}
+		pushTrackVis();
+	}else if(i == BTN_ENC_L){
+		auto save = new SaveState(this);
+		save->push(this);
+	}
+}
+
+void TrackEditState::doubleClick(uint8_t i){
+	if(i >= 5) return;
+
+	track.timeline.clear(cursor, i);
+	pushTrackVis();
+}
+
+void TrackEditState::buttonPressed(uint i){
+	if(i >= 5){
+		click(i);
+		return;
+	}
+
+	if(!clickTimes[i]){
+		click(i);
+	}else{
+		if(clickTimes[i] + doubleClickInterval >= millis()){
+			doubleClick(i);
+		}else{
+			click(i);
+		}
+	}
+	clickTimes[i] = millis();
+
+}
+
+void TrackEditState::pushTrackVis(){
+	TrackVisData data;
+	data.cursor = cursor;
+	data.timeline = track.timeline;
+
+	trackVis.push(data);
 }
--- src/States/TrackEditState.h
+++ src/States/TrackEditState.h
@@ -2,16 +2,50 @@
 #define SYNTHIA_FIRMWARE_TRACKEDITSTATE_H
 
 #include "State.h"
+#include <Synthia.h>
+#include <cstdlib>
 #include "../Model/Track.h"
+#include "../Visualization/TempoVisualizer.h"
+#include "../Visualization/VolumeVisualizer.h"
+#include "../Visualization/TrackVisualizer.h"
+#include "../ButtonActionSystem.h"
+#include <Input/InputListener.h>
 
-class TrackEditState : public State {
+class TrackEditState : public State, public EncoderListener, public SliderListener, public InputListener {
 public:
+	TrackEditState();
+
 	void setTrack(Track track);
-	Track& getTrack();
+	const Track& getTrack();
+
 protected:
 	void onStart() override;
 	void onStop() override;
+
+private:
+	void buttonPressed(uint i) override;
+	void buttonHeld(uint i) override;
+	void click(uint8_t i);
+	void doubleClick(uint8_t i);
+
+	void leftEncMove(int8_t amount) override;
+	void rightEncMove(int8_t amount) override;
+
+	void leftPotMove(uint8_t value) override;
+	void rightPotMove(uint8_t value) override;
+
 	Track track;
+	static uint8_t cursor;
+
+	TempoVisualizer tempoVis;
+	VolumeVisualizer volumeVis;
+	TrackVisualizer trackVis;
+
+	uint32_t clickTimes[5] = {0};
+	uint32_t doubleClickInterval = 500;//[ms]
+
+	void pushTrackVis();
+
 };
 
 
