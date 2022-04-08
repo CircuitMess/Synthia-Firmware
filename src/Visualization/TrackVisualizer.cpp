26193330ebaf589b68d85493e3020432d15a2816
Emil Gajšak <emil.gajsak@gmail.com>
2022-04-08 16:57:33 +0200

    Added TrackVisualizer dimmed cursor strip on TrackMatrix, LEDStrip stopAnimation before pushing

--- src/Visualization/TrackVisualizer.cpp
+++ src/Visualization/TrackVisualizer.cpp
@@ -4,10 +4,9 @@
 
 void TrackVisualizer::visualize(){
 	auto data = getProp();
-
 	for(int y = 0; y < Synthia.TrackMatrix.getHeight(); y++){
 		for(int x = 0; x < Synthia.TrackMatrix.getWidth(); x++){
-			uint8_t val = data.timeline.get(x, y) * 255;
+			uint8_t val = min(255, data.timeline.get(x, y) * 255 + (70 * (data.cursor == x)));
 			Synthia.TrackMatrix.drawPixel(x, y, { val, val, val, val });
 		}
 	}
