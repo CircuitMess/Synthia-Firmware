af108f5f2e363b48e834bda35e1a80a3d832cb9e
Filip Budiša <filip@circuitmess.com>
2022-04-13 18:11:44 +0200

    Add animation to VolumeVisualizer

--- src/Visualization/VolumeVisualizer.cpp
+++ src/Visualization/VolumeVisualizer.cpp
@@ -1,6 +1,11 @@
 #include "VolumeVisualizer.h"
 #include <Synthia.h>
+#include <SPIFFS.h>
 #include "LEDStrip.h"
+
+VolumeVisualizer::VolumeVisualizer() : anim(SPIFFS.open("/Volume.gif")){
+	anim.setMatrix(&Synthia.TrackMatrix);
+}
 
 void VolumeVisualizer::visualize(){
 	uint8_t value = getProp();
@@ -8,4 +13,12 @@ void VolumeVisualizer::visualize(){
 	//TODO - start volume anim on TrackMatrix
 	LEDStrip.setRight(value);
 	LEDStrip.setMidFill(value);
+}
+
+void VolumeVisualizer::onStart(){
+	anim.start();
+}
+
+void VolumeVisualizer::onStop(){
+	anim.stop();
 }
