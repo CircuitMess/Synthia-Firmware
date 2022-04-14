ee94aa52fa5cac614e41382e27b6a722093b9613
Filip Budiša <filip@circuitmess.com>
2022-04-14 09:59:42 +0200

    Add SampleSpeedVisualizer aimations

--- src/Visualization/SampleSpeedVisualizer.cpp
+++ src/Visualization/SampleSpeedVisualizer.cpp
@@ -1,12 +1,32 @@
+#include <SPIFFS.h>
+#include <FS/RamFile.h>
+#include <Synthia.h>
 #include "SampleSpeedVisualizer.h"
 #include "LEDStrip.h"
 
+SampleSpeedVisualizer::SampleSpeedVisualizer(){
+	File f = SPIFFS.open("/GIF/Speed.gif");
+	if(!f){
+		ESP_LOGE("SampleVis", "Can't open /GIF/Speed.gif");
+		return;
+	}
+	f = RamFile::open(f);
+
+	anim.reset(new MatrixAnimGIF(f));
+	anim->setMatrix(&Synthia.TrackMatrix);
+}
+
 void SampleSpeedVisualizer::visualize(){
-	//TODO - start speed anim on TrackMatrix
 	auto val = getProp();
 	LEDStrip.setLeftFromCenter((int8_t)((int)(val) - 128));
+	LEDStrip.setMidFillFromCenter((int8_t)((int)(val) - 128));
+}
+
+void SampleSpeedVisualizer::onStart(){
+	anim->start();
 }
 
 void SampleSpeedVisualizer::onStop(){
-	//TODO - stop anim
+	anim->stop();
+	LEDStrip.setMidFill(0);
 }
--- src/Visualization/SampleSpeedVisualizer.h
+++ src/Visualization/SampleSpeedVisualizer.h
@@ -1,12 +1,20 @@
 #ifndef SYNTHIA_FIRMWARE_SAMPLESPEEDVISUALIZER_H
 #define SYNTHIA_FIRMWARE_SAMPLESPEEDVISUALIZER_H
 
+#include <Devices/Matrix/MatrixAnimGIF.h>
 #include "Visualizer.h"
 
 class SampleSpeedVisualizer : public Visualizer<uint8_t> {
+public:
+	SampleSpeedVisualizer();
+
 protected:
 	void visualize() override;
 	void onStop() override;
+	void onStart() override;
+
+private:
+	std::unique_ptr<MatrixAnimGIF> anim;
 };
 
 
