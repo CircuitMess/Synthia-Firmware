33fd89a6c19de8dede2a4fe24ba22f3c85598f3c
Emil Gajšak <emil.gajsak@gmail.com>
2022-05-06 13:22:28 +0200

    Added samples generation script and sentences used

---
+++ scripts/sentences.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# Takes the input file sentences.txt and saves .mp3 files in the data folder
+# requires ffmpeg and jq installed
+
+# Google TTS api key
+KEY=
+
+while IFS= read -r sentence; do
+  dir=$(echo "$sentence" | cut -d " " -f1)
+  name=$(echo "$sentence" | cut -d " " -f2)
+  sentence=$(echo "$sentence" | tail -c +$(expr ${#dir} + ${#name} + 3))
+  mkdir -p "data/$dir"
+  echo "$dir/$name: $sentence"
+  curl -s --location --request POST "https://texttospeech.googleapis.com/v1/text:synthesize?key=$KEY" \
+    --header "Content-Type: application/json" \
+    --data-raw '{
+    "input": { "ssml": "'"$sentence"'" },
+    "voice": {
+        "languageCode": "en-US",
+        "name": "en-US-Standard-D",
+        "ssmlGender": "NEUTRAL"
+    },
+    "audioConfig": {
+        "audioEncoding": "MP3",
+        "speakingRate": 0.96,
+        "pitch": 5.5,
+        "sampleRateHertz": 16000
+    }
+  }' | jq -r '.audioContent' | base64 -d  | ffmpeg -loglevel quiet -f mp3 -i pipe: -c:a libmp3lame -b:a 24k "data/$dir/$name.mp3"
+done < sentences.txt
